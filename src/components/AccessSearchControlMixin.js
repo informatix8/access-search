1974df49a4d32c4becd9060bd9b7e9c1ea8cfd43
Mjr <mjr@informatix.io>
2020-01-14 13:09:20 -0500

    lint fix

--- src/components/AccessSearchControlMixin.js
+++ src/components/AccessSearchControlMixin.js
@@ -12,7 +12,7 @@ const AccessSearchControlMixin = {
         }
     },
     computed: {
-        componentId() {
+        componentId () {
             return this.sid + this.fieldType;
         }
     }
